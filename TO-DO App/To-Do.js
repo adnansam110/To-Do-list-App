c23588ffbe32fe49ac3427f5b9970be59511e79f
adnansam110 <51002433+adnansam110@users.noreply.github.com>
2020-12-31 00:12:05 +0500

    Update To-Do.js

--- TO-DO App/To-Do.js
+++ TO-DO App/To-Do.js
@@ -10,36 +10,44 @@ clr.addEventListener('click', clrList);
 function addList(e)
 {
     // --- Creating list and adding to list items ---
+     
     e.preventDefault();
-
-     var li = document.createElement('li');
+    var li = document.createElement('li');
 
     var newList = document.getElementById('item').value;
-
-    li.className = 'list-group-item';
-       
-    li.appendChild(document.createTextNode(newList));
-    //console.log(li);
-        
-    listItems.appendChild(li);
+    if (newList!=="")
     
-    // --- Delete button ---
-    var del = document.createElement('button');
+    {  
+        e.preventDefault();
+        li.className = 'list-group-item';
+        
+        li.appendChild(document.createTextNode(newList));
+        //console.log(li);
+            
+        listItems.appendChild(li);
+        
+        // --- Delete button ---
+        var del = document.createElement('button');
 
-    del.className = 'btn btn-danger btn-sm float-right delete';
+        del.className = 'btn btn-danger btn-sm float-right delete';
 
-    del.appendChild(document.createTextNode('X'));
+        del.appendChild(document.createTextNode('X'));
 
-    li.appendChild(del);
+        li.appendChild(del);
 
-    // --- Check Button ---
-    var chk = document.createElement('button');
+        // --- Check Button ---
+        var chk = document.createElement('button');
 
-    chk.className = 'btn btn-success btn-sm float-right check';
+        chk.className = 'btn btn-success btn-sm float-right check';
 
-    chk.appendChild(document.createTextNode('Check'));
+        chk.appendChild(document.createTextNode('Check'));
 
-    li.appendChild(chk);
+        li.appendChild(chk);
+    }
+    else
+    {
+        alert("Please input something!")
+    }
 }
 
 function checkOrDel(e){
@@ -64,12 +72,11 @@ function checkOrDel(e){
 }
 function clrList(e)
 {
-    // listItems.style.display= 'none';
-    
-    listItems.remove();
-    // var ul = document.createElement('ul');
-    // ul.className = 'list-group';
-    // ul.id= 'items';
-    // console.log(ul);
+     //listItems.style.display= 'none';
+     //e.preventDefault();
+     listItems.remove();
+     
+     
+     
     
 }
